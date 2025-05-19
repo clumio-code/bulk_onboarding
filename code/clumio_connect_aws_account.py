1e4bafd31cf7df86f0edd611015544605a3b2a9b
dahye kim <dahye@clumio.com>
2025-05-19 16:46:01 +0900

    Double quote to single quote.
    Signed-off-by: dahye kim <dahye@clumio.com>

--- code/clumio_connect_aws_account.py
+++ code/clumio_connect_aws_account.py
@@ -1,13 +1,13 @@
 # Copyright 2025. Clumio, a Commvault Company.
 
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
 
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -20,21 +20,21 @@ logger = logging.getLogger(__name__)
 
 def lambda_handler(events, context):
     # Parse the input event
-    bear = events.get("bear")
-    api_url = events.get("api_url")
-    stack_name = events.get("stack_name", None)
-    account = events.get("account", None)
-    cft_admin_role = events.get("cft_admin_role", 'AWSCloudFormationStackSetAdministrationRole')
+    bear = events.get('bear')
+    api_url = events.get('api_url')
+    stack_name = events.get('stack_name', None)
+    account = events.get('account', None)
+    cft_admin_role = events.get('cft_admin_role', 'AWSCloudFormationStackSetAdministrationRole')
     if account:
-        account_id_list = account.get("aws_account_id_list", None)
-        region_list = account.get("aws_region_list", None)
-        aws_service_list = account.get("aws_service_list", None)
+        account_id_list = account.get('aws_account_id_list', None)
+        region_list = account.get('aws_region_list', None)
+        aws_service_list = account.get('aws_service_list', None)
     else:
-        raise clumio_sdk_v8c.InvalidInputException("No account detail provided.")
+        raise clumio_sdk_v8c.InvalidInputException('No account detail provided.')
     if not all((account_id_list, region_list, aws_service_list)):
         raise clumio_sdk_v8c.InvalidInputException(
-            "Missing account detail. Make sure to provide account_id_list, region_list, "
-            f"and aws_service_list for account {account}."
+            'Missing account detail. Make sure to provide account_id_list, region_list, '
+            f'and aws_service_list for account {account}.'
         )
 
     deploy_region = region_list[0]
@@ -51,9 +51,9 @@ def lambda_handler(events, context):
 
         # Parse the Clumio token, URL, and id to run the Clumio deployment stack
         rsp = clumio_connect_api.create_connection_group()
-        deployment_template_url_clumio = rsp.get("deployment_template_url")
-        clumio_token = rsp.get("id")
-        external_id = rsp.get("external_id")
+        deployment_template_url_clumio = rsp.get('deployment_template_url')
+        clumio_token = rsp.get('id')
+        external_id = rsp.get('external_id')
 
         # Deploy CFT stack to connect AWS account to Clumio
         aws_account_mng.run_clumio_deploy_stack(
--- code/clumio_sdk_v8c.py
+++ code/clumio_sdk_v8c.py
@@ -1,13 +1,13 @@
 # Copyright 2025. Clumio, a Commvault Company.
 
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
 
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -21,7 +21,6 @@ import functools
 import string
 from botocore.exceptions import ClientError
 import api_dict
-from mypy_boto3_sts import type_defs as sts_types
 
 API_DICT = api_dict.API_DICT
 
@@ -41,7 +40,7 @@ class InvalidInputException(Exception):
 class API:
     def __init__(self, id: str, url: str, token: str):
         self._id = id
-        self._url_prefix = f"{url}/"
+        self._url_prefix = f'{url}/'
         self._token = token
         self._payload: dict | None = None
 
@@ -51,55 +50,55 @@ class API:
 
     @property
     def version(self):
-        return API_DICT[self._id]["version"]
+        return API_DICT[self._id]['version']
 
     @property
     def type(self):
-        return API_DICT[self._id]["type"]
+        return API_DICT[self._id]['type']
 
     @property
     def header(self):
-        accept_header = API_DICT[self._id]["header"]
-        header_dict = {"accept": accept_header, "authorization": f"Bearer {self.token}"}
-        if self.type == "post":
-            header_dict["content-type"] = "application/json"
+        accept_header = API_DICT[self._id]['header']
+        header_dict = {'accept': accept_header, 'authorization': f'Bearer {self.token}'}
+        if self.type == 'post':
+            header_dict['content-type'] = 'application/json'
         return header_dict
 
     @property
     def url(self):
-        return self._url_prefix + API_DICT[self._id]["api"]
+        return self._url_prefix + API_DICT[self._id]['api']
 
     @property
     def query_parms(self):
-        return API_DICT[self._id]["query_parms"]
+        return API_DICT[self._id]['query_parms']
 
     @property
     def body_parms(self):
-        return API_DICT[self._id]["body_parms"]
+        return API_DICT[self._id]['body_parms']
 
     @property
     def return_code(self):
-        return API_DICT[self._id]["success"]
+        return API_DICT[self._id]['success']
 
     def exec_api(self) -> dict:
-        print("API Request: %s" % API_DICT[self._id]["desc"])
-        print(f"{self.type} API Request to {self.url} with payload {self._payload}")
+        print('API Request: %s' % API_DICT[self._id]['desc'])
+        print(f'{self.type} API Request to {self.url} with payload {self._payload}')
 
-        if self.type == "get":
+        if self.type == 'get':
             res = requests.get(self.url, headers=self.header)
-        elif self.type == "post":
+        elif self.type == 'post':
             res = requests.post(self.url, json=self._payload, headers=self.header)
 
         res_text_dict: dict = json.loads(res.text)
         if res.status_code != self.return_code:
-            status_msg = f"Unexpected API status: {res.status_code}"
+            status_msg = f'Unexpected API status: {res.status_code}'
             error_msg = (
-                f"status: {status_msg}, msg: {res_text_dict.get("errors", None)}"
+                f'status: {status_msg}, msg: {res_text_dict.get('errors', None)}'
             )
             raise ClumioException(error_msg)
 
         print(
-            f"API response: {res}, started task: {res_text_dict.get("task_id", None)}"
+            f'API response: {res}, started task: {res_text_dict.get('task_id', None)}'
         )
         return res_text_dict
 
@@ -114,7 +113,7 @@ class ClumioConnectAccount(API):
         regions: list[str],
         services: list[str],
     ):
-        super(ClumioConnectAccount, self).__init__("008", url, token)
+        super(ClumioConnectAccount, self).__init__('008', url, token)
         self.aws_account_to_connect: str | None = None
         self.master_aws_region: str | None = None
         self.master_aws_account_id: str | None = None
@@ -132,16 +131,16 @@ class ClumioConnectAccount(API):
         self.aws_region_list_to_connect = regions
         self.asset_types_to_enable = services
         self._payload = {
-            "account_native_id": self.aws_account_to_connect,
-            "master_region": self.master_aws_region,
-            "master_aws_account_id": self.master_aws_account_id,
-            "aws_regions": self.aws_region_list_to_connect,
-            "asset_types_enabled": self.asset_types_to_enable,
-            "template_permission_set": "all",
+            'account_native_id': self.aws_account_to_connect,
+            'master_region': self.master_aws_region,
+            'master_aws_account_id': self.master_aws_account_id,
+            'aws_regions': self.aws_region_list_to_connect,
+            'asset_types_enabled': self.asset_types_to_enable,
+            'template_permission_set': 'all',
         }
 
     def create_connection_group(self):
-        self._id = "008"
+        self._id = '008'
         return self.exec_api()
 
 
@@ -155,7 +154,7 @@ class AWSOrgAccount:
     ):
         self._account_id = account_id
         self._aws_region = region
-        self._rnd_string = "".join(random.choices(string.ascii_letters, k=5))
+        self._rnd_string = ''.join(random.choices(string.ascii_letters, k=5))
 
         # Get the current session information
         sts_client = boto3.client('sts')
@@ -185,26 +184,26 @@ class AWSOrgAccount:
         clumio_token: str,
         stack_name: str,
     ):
-        print(f"Deploying stack {stack_name} to {child_account_id}")
-        cft_client = self.session.client("cloudformation")
+        print(f'Deploying stack {stack_name} to {child_account_id}')
+        cft_client = self.session.client('cloudformation')
         try:
             deploy_rsp = cft_client.create_stack(
-                StackName=f"{stack_name}-{self._rnd_string}",
+                StackName=f'{stack_name}-{self._rnd_string}',
                 TemplateURL=template_url,
                 Parameters=[
-                    {"ParameterKey": "ClumioToken", "ParameterValue": clumio_token},
-                    {"ParameterKey": "RoleExternalId", "ParameterValue": external_id},
+                    {'ParameterKey': 'ClumioToken', 'ParameterValue': clumio_token},
+                    {'ParameterKey': 'RoleExternalId', 'ParameterValue': external_id},
                     {
-                        "ParameterKey": "PermissionModel",
-                        "ParameterValue": "SELF_MANAGED",
+                        'ParameterKey': 'PermissionModel',
+                        'ParameterValue': 'SELF_MANAGED',
                     },
-                    {"ParameterKey": "PermissionsBoundaryARN", "ParameterValue": ""},
+                    {'ParameterKey': 'PermissionsBoundaryARN', 'ParameterValue': ''},
                 ],
-                Capabilities=["CAPABILITY_NAMED_IAM"],
+                Capabilities=['CAPABILITY_NAMED_IAM'],
                 DisableRollback=True,
                 TimeoutInMinutes=60,
             )
-            print(f"deploy_status: {deploy_rsp}")
+            print(f'deploy_status: {deploy_rsp}')
         except ClientError as err:
-            error_msg = f"Failed to deploy stack {stack_name}: {err.response['Error']}"
+            error_msg = f'Failed to deploy stack {stack_name}: {err.response['Error']}'
             raise AWSException(error_msg) from err
